b5514e31b57ff8ea3bc77caf3ac0e4d3b5222919
Marcos <exegeses@gmail.com>
2021-09-23 17:16:12 -0300

    vista reporte de alta de una marca (sin terminar)

---
+++ catalogo/agregarMarca.php
@@ -0,0 +1,26 @@
+<?php
+    //require 'config/config.php';
+    include 'includes/header.html';
+    include 'includes/nav.php';
+?>
+
+    <main class="container">
+        <h1>Alta de una marca</h1>
+
+        <div class="alert alert-success col-7 mx-auto">
+            Marca agregada correctamente
+            <a href="adminMarcas.php" class="btn btn-light m-2">
+                Volver a panel de marcas
+            </a>
+        </div>
+
+        <div class="alert alert-danger col-7 mx-auto">
+            No se pudo agregar la marca.
+            <a href="adminMarcas.php" class="btn btn-light m-2">
+                Volver a panel de marcas
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
