845832636f7f16889e982519a7af82c62ffc3640
Marcos <exegeses@gmail.com>
2021-09-28 15:10:57 -0300

    Reporte de alta de una marca

--- catalogo/agregarMarca.php
+++ catalogo/agregarMarca.php
@@ -3,6 +3,12 @@
     require 'funciones/conexion.php';
     require 'funciones/marcas.php';
     $chequeo = agregarMarca();
+    $css = 'danger';
+    $mensaje = 'No se pudo agregar la marca.';
+    if( $chequeo ){
+        $css = 'success';
+        $mensaje = 'Marca agregada correctamente';
+    }
     include 'includes/header.html';
     include 'includes/nav.php';
 ?>
@@ -10,15 +16,8 @@
     <main class="container">
         <h1>Alta de una marca</h1>
 
-        <div class="alert alert-success col-7 mx-auto">
-            Marca agregada correctamente
-            <a href="adminMarcas.php" class="btn btn-light m-2">
-                Volver a panel de marcas
-            </a>
-        </div>
-
-        <div class="alert alert-danger col-7 mx-auto">
-            No se pudo agregar la marca.
+        <div class="alert alert-<?= $css ?> col-7 mx-auto">
+            <?= $mensaje ?>
             <a href="adminMarcas.php" class="btn btn-light m-2">
                 Volver a panel de marcas
             </a>
