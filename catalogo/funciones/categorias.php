a29164490994ccb15a8d5cdc22f4e6d181d20b76
Marcos <exegeses@gmail.com>
2021-10-05 14:39:48 -0300

    CRUD de categorías

--- catalogo/funciones/categorias.php
+++ catalogo/funciones/categorias.php
@@ -1,17 +1,89 @@
 <?php
 
-    ##############################
-    ####### CRUD de categorías  ######
+    ###############################
+    ######  CRUD de categorias
 
     function listarCategorias()
     {
-        //conexión
         $link = conectar();
-        //mensaje SQL
         $sql = "SELECT idCategoria, catNombre
                     FROM categorias";
-        //ejecución
-        $resultado = mysqli_query($link, $sql)
+        $categorias = mysqli_query( $link, $sql )
                         or die( mysqli_error($link) );
+        return $categorias;
+    }
+
+    function verCategoriaPorID()
+    {
+        $link = conectar();
+        $idCategoria = $_GET['idCategoria'];
+        $sql = "SELECT idCategoria, catNombre
+                        FROM categorias
+                        WHERE idCategoria = ".$idCategoria;
+        $resultado = mysqli_query( $link, $sql )
+                            or die( mysqli_error($link) );
+        $categoria = mysqli_fetch_assoc($resultado);
+        return $categoria;
+    }
+
+    function agregarCategoria()
+    {
+        $catNombre = $_POST['catNombre'];
+        $link = conectar();
+        $sql = "INSERT INTO categorias
+                            VALUES
+                                ( DEFAULT, '".$catNombre."' )";
+        $resultado = mysqli_query( $link, $sql )
+                                or die( mysqli_error($link) );
+        return $resultado;
+    }
+
+    /**
+     * función para chequear si hay uno o más productos
+     * de una categoría
+     * @return bool
+     *
+     */
+    function productoPorCategoria()
+    {
+        $idCategoria = $_GET['idCategoria'];
+        $link = conectar();
+        $sql = "SELECT 1 FROM productos
+                        WHERE idCategoria = ".$idCategoria;
+        $resultado = mysqli_query($link, $sql)
+                        or die(mysqli_error($link));
+        $cantidad = mysqli_num_rows($resultado);
+        return  $cantidad;
+    }
+
+    /**
+     * función para modificar una categoría
+     * @return $resultado:bool
+     */
+    function modificarCategoria()
+    {
+        $idCategoria = $_POST['idCategoria'];
+        $catNombre = $_POST['catNombre'];
+        $link = conectar();
+        $sql = "UPDATE categorias
+                            SET catNombre = '".$catNombre."'
+                            WHERE idCategoria = ".$idCategoria;
+        $resultado = mysqli_query($link, $sql)
+                        or die(mysqli_error($link));
+        return $resultado;
+    }
+
+    /**
+     * Función para eliminar una categoría
+     * @return bool|mysqli_result
+     */
+    function eliminarCategoria()
+    {
+        $idCategoria = $_POST['idCategoria'];
+        $link = conectar();
+        $sql = "DELETE categorias 
+                            WHERE idCategoria = ".$idCategoria;
+        $resultado = mysqli_query($link, $sql)
+                        or die(mysqli_error($link));
         return $resultado;
     }
